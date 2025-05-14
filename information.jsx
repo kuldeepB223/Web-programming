d133a7a7e498832fad1b32b23d8cfa6daa396c29
kuldeepB223 <kuldeepbadgujar223@gmail.com>
2025-05-15 00:37:32 +0530

    question 4 Using function component
    question 4 Using function component

--- information.jsx
+++ information.jsx
@@ -1,17 +1,29 @@
 import { useState } from "react";
-import imgs from './logo.svg'
-function App(){
 
-    const[image, setimage] = useState(false);
- 
-    function hideimg(){
-        setimage(!image);
+function App(){
+    const[studentName,setStudentName]=useState([]);
+    const[changeName,setChangeName]=useState([]);
+
+    function addStudent(){
+        let students=[...studentName];
+        students.push(changeName);
+        setStudentName(students);
     }
-    return(
-        <>
-        <img hidden={image} src={imgs} height={200} width={200}></img>
-        <input type="checkbox" onClick={hideimg} checked={!image}></input>
-        </>
-    )
+
+    function addName(e){
+        setChangeName(e.target.value);
+    }
+    return (
+        <div>
+            <input type="text" onChange={addName}></input>
+            <button onClick={addStudent}>Add Student</button>
+            <ul>
+                {studentName.map((s)=>{
+                    return <li>{s}</li>;
+                })}
+            </ul>
+        </div>
+    );
 }
+
 export default App;
