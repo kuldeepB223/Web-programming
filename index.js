1ce5d8cefc829f69b5ad0637df851bccdca83711
kuldeepB223 <kuldeepbadgujar223@gmail.com>
2025-05-15 00:38:47 +0530

    Question 1 dealing with rest API  Using  class component
    Question 1 dealing with rest API  Using  class component

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 import ReactDOM from 'react-dom/client';
-import App from './information';
+import Information from './information';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
+root.render(<Information/>);
 
--- information.jsx
+++ information.jsx
@@ -1,29 +1,60 @@
-import { useState } from "react";
+import { Component } from "react";
+ import '../node_modules/bootstrap/dist/css/bootstrap.css'
+class Information extends Component {
 
-function App(){
-    const[studentName,setStudentName]=useState([]);
-    const[changeName,setChangeName]=useState([]);
+    state = { users : []} 
 
-    function addStudent(){
-        let students=[...studentName];
-        students.push(changeName);
-        setStudentName(students);
+     componentDidMount()
+    {
+        debugger;
+        var helper = new XMLHttpRequest();
+        helper.onreadystatechange =()=>{
+            if(helper.readyState === 4 && 
+                helper.status === 200
+            )
+            {
+                
+                debugger;
+                var dataReceivedInString = helper.responseText;
+                var dataInJSONFormat = JSON.parse(dataReceivedInString);
+                var usersFromServer = dataInJSONFormat;
+                this.setState({users: usersFromServer});
+            }
+        };
+        helper.open("GET","http://localhost:3000/data.json")
+        helper.send();
     }
 
-    function addName(e){
-        setChangeName(e.target.value);
-    }
-    return (
-        <div>
-            <input type="text" onChange={addName}></input>
-            <button onClick={addStudent}>Add Student</button>
-            <ul>
-                {studentName.map((s)=>{
-                    return <li>{s}</li>;
-                })}
-            </ul>
+    render() { 
+        return (
+        <div className="table-responsive">
+            <table className="table table-bordered">
+                <thead>
+                    <th>User Id</th>
+                    <th>Name</th>
+                    <th>username</th>
+                    <th>Email</th>
+                    <th>Address</th>
+                      
+                </thead>
+
+                <tbody>
+                    {
+                        this.state.users.map(user=>{
+                        return    <tr>
+                        <td>{user.id}</td>
+                        <td>{user.name}</td>
+                        <td>{user.username}</td>
+                        <td>{user.email}</td>
+                        <td>{user.address.zipcode}</td>
+                         </tr>
+                        })       
+                    }
+                </tbody>
+            </table>
         </div>
-    );
+        );
+    }
 }
-
-export default App;
+ 
+export default Information;
